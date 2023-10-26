ebff3513d520d57428d7b0107d3da3c2e1db6357
kyrheavy@gmail.com <kyrheavy@gmail.com>
2023-10-26 17:36:32 -0400

    first attempt of Etch-A-Sketch with reset button working on grids up to 50 pixels

---
+++ script.js
@@ -0,0 +1,48 @@
+function createGrid(gridSize){
+    const gridContainer = document.querySelector(".gridContainer");
+
+    gridContainer.innerHTML = "";
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+
+    for(let row = 0; row < gridSize; row++){
+        for(let col = 0; col < gridSize; col++){
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("gridItem");
+            gridContainer.appendChild(gridItem);
+        }
+    }
+    
+    addHoverEffect();
+};
+
+function addHoverEffect(){
+    const gridItem = document.querySelectorAll(".gridItem");
+    
+    gridItem.forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            item.classList.add("hovered");
+        });
+        
+        /* item.addEventListener("mouseleave", () => {
+            item.classList.remove("hovered");
+        }); */
+    });
+};
+
+const maxGridSize = 50;
+
+function resetGrid() {
+    const newSize = prompt(`Enter new grid size up to ${maxGridSize}:`);
+    if(newSize){
+        const validatedSize = Math.min(parseInt(newSize, 10), maxGridSize);
+        createGrid(validatedSize);
+    };
+};
+
+createGrid(16);
+
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", resetGrid);
